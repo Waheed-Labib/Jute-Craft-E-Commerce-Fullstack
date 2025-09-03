1a0f1410289d3e5e1a6a0605aaef3634dce1f2c6
Waheed-Labib <labibwahid28@gmail.com>
2025-09-03 23:08:20 +0600

    setup & installation

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#1DA1F2",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
