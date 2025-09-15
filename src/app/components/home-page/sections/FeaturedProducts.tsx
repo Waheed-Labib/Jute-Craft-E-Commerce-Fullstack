f9cd0801e29abe6434f2e1c70208fd4e34a931e9
Waheed-Labib <labibwahid28@gmail.com>
2025-09-15 22:02:12 +0600

    updated FeaturedProducts.tsx

--- src/app/components/home-page/sections/FeaturedProducts.tsx
+++ src/app/components/home-page/sections/FeaturedProducts.tsx
@@ -52,7 +52,7 @@ export default function FeaturedProducts() {
                 </SectionHeadingLight>
             </div>
 
-            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+            <div className="grid gap-12 sm:grid-cols-2 lg:grid-cols-3">
                 {products.map((product, idx) => (
                     <ProductCard key={idx} {...product} />
                 ))}
